48a5526756e0f8ae855508a85cd383788aa11587
Ryan VanAlstine <rtvanals@gmail.com>
2017-09-01 16:17:57 -0500

    Update package file

--- Package.swift
+++ Package.swift
@@ -4,9 +4,8 @@ let package = Package(
     name: "S3SignerAWS",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/vapor/crypto.git",
-                 majorVersion: 1),
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2)
         
         
         ]
